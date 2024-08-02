f46257123eaf48b5d688363db1ec447a8bd983bd
Rishab Kumar <rishabkumar7@gmail.com>
2024-08-02 10:45:19 -0600

    added dockerfiles

--- api/main.py
+++ api/main.py
@@ -29,7 +29,7 @@ s3 = boto3.client(
     aws_access_key_id= os.getenv("AWS_ACCESS_KEY"),
     aws_secret_access_key= os.getenv("AWS_SECRET_KEY"))
 
-bucket_name = 'YOUR_BUCKET_NAME' # Add your bucket name here
+bucket_name = 'qrcode-storage-devops-capstone' # Add your bucket name here
 
 @app.post("/generate-qr/")
 async def generate_qr(url: str):
